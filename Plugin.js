477393a08e311b77d1047d928a2043cbe77a7b8c
NaamloosDT <ryandejonge@outlook.com>
2016-12-13 15:44:38 +0100

    Support voor custom css link (thema knop op login)

--- Plugin.js
+++ Plugin.js
@@ -25,6 +25,10 @@
 
 var main = function() {
     var css = GM_getValue("magister_theme", "https://naamloosdt.github.io/MagisterTheme/style/style.css");
+    if(css == "null" || css === null){
+        GM_setValue("magister_theme", "https://naamloosdt.github.io/MagisterTheme/style/style.css");
+    }
+    css = GM_getValue("magister_theme", "https://naamloosdt.github.io/MagisterTheme/style/style.css");
     try{
         var bg = document.getElementsByTagName('mg-background-carrousel')[0];
         bg.parentNode.removeChild(bg);
@@ -35,3 +39,14 @@ var main = function() {
 };
 
 $(document).ready(main);
+
+$('<a class="btn-primary3" ng-class="{\'state-busy\': isBusy}" data-ng-click="" id="thema" tabindex="3">Thema</a>').appendTo('body');
+
+function setthema(url){
+    GM_setValue("magister_theme", url);
+}
+
+$("#thema").click(function(){
+    var url = prompt("Stel thema URL in", GM_getValue("magister_theme", "https://naamloosdt.github.io/MagisterTheme/style/style.css"));
+    setthema(url);
+});
